9c36b35bc5b06c234c1245e7d44e4e9501f338b7
hengfeng.zhuo <hengfeng.zhuo@rokid.com>
2019-07-06 14:01:01 +0800

    [add] 提交ffmpeg头文件和库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_ryan_videoplayer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Native C++";
     return env->NewStringUTF(hello.c_str());
 }
